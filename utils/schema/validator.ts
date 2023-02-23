24b15aa9136185baeddb37e7f82ff18f2ea9a412
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2023-02-23 22:33:22 +0300

    Update validator.ts

--- utils/schema/validator.ts
+++ utils/schema/validator.ts
@@ -10,16 +10,11 @@ type ValidateSchemaProps<T> = {
 
 export const validateSchema = async <T>({ schema, json }: ValidateSchemaProps<T>) => {
   await test.step('Validating json schema', async () => {
-    const prettyJson = JSON.stringify(json, null, 2);
-    const prettySchema = JSON.stringify(schema, null, 2);
+    const validate = ajv.compile(schema);
 
-    await test.step(`Schema: ${prettySchema}\nJSON: ${prettyJson}`, async () => {
-      const validate = ajv.compile(schema);
-
-      if (!validate(json)) {
-        const prettyError = JSON.stringify(validate.errors, null, 2);
-        throw Error(`Schema validation error: ${prettyError}`);
-      }
-    });
+    if (!validate(json)) {
+      const prettyError = JSON.stringify(validate.errors, null, 2);
+      throw Error(`Schema validation error: ${prettyError}`);
+    }
   });
 };
