3a5a8aa92bcd718021a7c31952ca48fe4d173b85
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2023-02-24 13:03:21 +0300

    Update environment.d.ts

--- environment.d.ts
+++ environment.d.ts
@@ -1,9 +1,12 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      CI: string;
+      ENV_NAME: string;
       BASE_URL: string;
       TEST_USER_EMAIL: string;
       TEST_USER_PASSWORD: string;
+      ALLURE_RESULTS_FOLDER: string;
     }
   }
 }
